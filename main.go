f63e384622fb569e7e73dd35c95590db63c12f34
Andrew Onyshchuk <andryk.rv@gmail.com>
2023-07-27 12:00:56 -0700

    init

---
+++ main.go
@@ -0,0 +1,81 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"strconv"
+	"strings"
+	"time"
+
+	"github.com/araddon/dateparse"
+)
+
+func parse(s string) (time.Time, error) {
+	if s == "" || s == "now" {
+		return time.Now(), nil
+	}
+
+	if strings.HasPrefix(s, "now-") {
+		d, err := time.ParseDuration(s[4:])
+		if err != nil {
+			return time.Time{}, err
+		}
+		return time.Now().Add(-d), nil
+
+	}
+
+	if d, err := time.ParseDuration(s); err == nil {
+		return time.Now().Add(d), nil
+	}
+
+	return dateparse.ParseLocal(s)
+}
+
+var stepFlag = flag.Duration("s", 10*time.Minute, "step")
+
+var formatFlag = flag.String("f", "s", "format")
+
+func format(t time.Time) string {
+	switch *formatFlag {
+	case "s":
+		return strconv.FormatInt(t.Unix(), 10)
+	case "ms":
+		return strconv.FormatInt(t.UnixMilli(), 10)
+	case "rfc":
+		return t.Format(time.RFC3339)
+	case "dt":
+		return t.Format(time.DateTime)
+	default:
+		panic("unknown format")
+	}
+}
+
+func main() {
+	flag.Parse()
+	if !flag.Parsed() {
+		panic("arg error")
+	}
+	start, err := parse(flag.Arg(0))
+	if err != nil {
+		panic(err)
+	}
+
+	end, err := parse(flag.Arg(1))
+	if err != nil {
+		panic(err)
+	}
+
+	step := *stepFlag
+
+	for ts := start.Round(step); ts.Before(end); ts = ts.Add(step) {
+		t1 := ts
+		t2 := ts.Add(step)
+		if t2.After(end) {
+			t2 = end
+		}
+
+		fmt.Println(format(t1), format(t2))
+
+	}
+
+}
